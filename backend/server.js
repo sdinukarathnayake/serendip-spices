7d46e2d6dd4834c4aab0a4b370080b5dbf3e8bcf
DR <it22140616@my.sliit.lk>
2025-06-25 09:51:24 +0530

    Updated server.js file
    - Installed CORS
    - Created .env file
    - Updated server.js file

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,25 @@
-const express = require('express');
+// Loads environment variables from a .env file into process.env
+const dotenv = require("dotenv");
+dotenv.config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors"); // Cross-Origin Resource Sharing (so API can be called from other domains).
+
 const app = express();
+const port = process.env.PORT;
 
-const port = 4100;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-app.listen(port, () => {
-    console.log(`Server running on port : ${port}`);
+async function start() {
+    app.listen(port, () =>
+        console.log(`Server running on http://localhost:${port}`)
+    );
+}
+
+start().catch(err => {
+    console.error(err);
+    process.exit(1);
 });
