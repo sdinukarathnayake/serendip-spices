a12c17343faa7d7a72f9daa4c23e8dc0410585b8
RashmiFernando <it22177414@my.sliit.lk>
2025-06-26 22:58:21 +0530

    Updated product controller class

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const{createProduct, viewAllProucts,viewProuct,updateProduct,deleteProduct} = require('../controllers/productController');
+const{createProduct, viewAllProducts,viewProduct,updateProduct,deleteProduct} = require('../controllers/productController');
 
 router.post('/', createProduct);
-router.get('/', viewAllProucts);
-router.get('/id/:productId', viewProuct);
+router.get('/', viewAllProducts);
+router.get('/id/:productId', viewProduct);
 router.put('/id/:productId', updateProduct);
 router.delete('/id/:productId', deleteProduct);
+
 module.exports = router;
