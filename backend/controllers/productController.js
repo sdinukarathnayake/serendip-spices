7de47621a32d93e4ddf99208991eb53f38bbc444
DR <it22140616@my.sliit.lk>
2025-06-27 08:02:17 +0530

    Corrected CRUD related to Product
    - Updated server.js formatting
    - Updated product model formatting
    - Added ";" in product controller
    - Updated order model
    - Corrected deleteProduct function in product controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,20 +2,19 @@ const Product = require('../modules/productModel');
 
 const createProduct = async (req,res) =>{
 
-    try{
-         const{productId, productName, pricePerKg, sellerId, sellerName, description, origin, catalogue} = req.body;
+    try {
+        const{productId, productName, pricePerKg, sellerId, sellerName, description, origin, catalogue} = req.body;
 
         if(!productId || !productName|| !pricePerKg|| !sellerId|| !sellerName|| !description|| !origin|| !catalogue){
            return  res.status(400).json({message: "Missing required fields."});
         };
 
         const newProduct = new Product ({productId, productName, pricePerKg, sellerId, sellerName, description, origin, catalogue});
-    await newProduct.save();
-    res.status(201).json({ message : "New product created successfully", Product: newProduct});
-    
+        await newProduct.save();
+        res.status(201).json({ message : "New product created successfully", Product: newProduct});
     }
     catch (err){
-res.status(500) .json({message : "Failed to create product"});
+        res.status(500) .json({message : "Failed to create product"});
     }
 };
 
@@ -44,38 +43,37 @@ const viewProduct = async (req, res) => {
 };
 
 const updateProduct = async (req, res) =>{
- try{
-    const {productId} = req.params;
-const { productName, pricePerKg, sellerId, sellerName, description, origin, catalogue}= req.body;
+    try{
+        const {productId} = req.params;
+        const { productName, pricePerKg, sellerId, sellerName, description, origin, catalogue}= req.body;
 
-const product = await Product.findOne({productId});
+        const product = await Product.findOne({productId});
 
+        if(!product){
+            return res.status(404).json({ message: "Product not found"});
+        };
 
-if(!product){
-    return res.status(404).json({ message: "Product not found"});
-};
+        const updateProduct = await Product.findOneAndUpdate(
+            {productId}, {productId, productName, pricePerKg, sellerId, sellerName, description, origin, catalogue}, {new: true}
+        );
 
-const updateProduct = await Product.findOneAndUpdate(
-    {productId}, {productId, productName, pricePerKg, sellerId, sellerName, description, origin, catalogue}, {new: true}
-);
-
-res.status(200).json ({message:"Product updated successfully", Product: updateProduct});
-}
- catch(err){
-    res.status(500).json ({message : " Failed to update product", error: err.message});
- }
+        res.status(200).json ({message:"Product updated successfully", Product: updateProduct});
+    }
+    catch (err){
+        res.status(500).json ({message : " Failed to update product", error: err.message});
+    }
 };
 
 
 const deleteProduct = async (req, res) => {
     try {
         const {productId} = req.params;
-        const product = await product.findOneAndDelete({productId});
-        res.status(200).json({ message: "Product deleted successfully", Product: deleteProduct });
+        const deletedProduct = await Product.findOneAndDelete({productId});
+        res.status(200).json({ message: "Product deleted successfully", Product: deletedProduct });
     }
     catch (err) {
         res.status(500).json({ message: 'Failed to delete product', error: err.message });
     }
 };
 
-module.exports = {createProduct, viewAllProducts,viewProduct,updateProduct,deleteProduct}
+module.exports = {createProduct, viewAllProducts, viewProduct, updateProduct, deleteProduct};
