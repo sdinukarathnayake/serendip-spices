4594faeee56139cb9f5439f0a63b9fcbe11cc6bc
RashmiFernando <it22177414@my.sliit.lk>
2025-07-02 13:33:48 +0530

    Updated backend files

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,80 +1,82 @@
 const Order = require('../modules/orderModel');
 
-const createOrder = async(req,res) =>{
+const createOrder = async (req, res) => {
 
-    try{
-        const{orderId, productId, sellerId, buyerId, soldPrice, quantity, total, deliveryAddressLine1, deliveryAddressLine2, deliveryCity, contactNumber, orderDate, orderTime, orderStatus} = req.body;
+    try {
+        const { orderId, productId, sellerId, buyerId, soldPrice, quantity, total, deliveryAddressLine1, deliveryAddressLine2, deliveryCity, contactNumber, orderStatus } = req.body;
 
-        if(!orderId || !productId || !sellerId || !buyerId || !soldPrice || !quantity || !total || !deliveryAddressLine1 || !deliveryAddressLine2 || !deliveryCity || !contactNumber || !orderTime || !orderStatus){
-            return res.status(400).json({message: "Missing required fields."});
+        if (!orderId || !productId || !sellerId || !buyerId || !soldPrice || !quantity || !total || !deliveryAddressLine1 || !deliveryAddressLine2 || !deliveryCity || !contactNumber || !orderStatus) {
+            return res.status(400).json({ message: "Missing required fields." });
         };
 
-        const newOrder = new Order({orderId, productId, sellerId, buyerId, soldPrice, quantity, total, deliveryAddressLine1, deliveryAddressLine2, deliveryCity, contactNumber, orderDate, orderTime, orderStatus});
-    await newOrder.save();
-    res.status(201).json({message: "New order created successfully", Order: newOrder});
+        const newOrder = new Order({ orderId, productId, sellerId, buyerId, soldPrice, quantity, total, deliveryAddressLine1, deliveryAddressLine2, deliveryCity, contactNumber, orderStatus });
+        await newOrder.save();
+
+        res.status(201).json({ message: "New order created successfully", Order: newOrder });
     }
-    catch(err){
-        res.status(500).json({message:"Failed to create order", error: err.message});
+    catch (err) {
+        res.status(500).json({ message: "Failed to create order", error: err.message });
     }
 };
 
 const viewAllOrders = async (req, res) => {
-    try{
+    try {
         const orders = await Order.find();
         res.status(200).json(orders);
     }
-    catch(err){
-        res.status(500).json({message: "Failed to fetch orders"});
+    catch (err) {
+        res.status(500).json({ message: "Failed to fetch orders" });
     }
 };
 
-const viewOrder = async(req, res) => {
-    try{
-        const {orderId} = req.params;
-        const order = await Order.findOne({orderId});
-
-        if(!order) return res.status(404).json({message : "Order not found"});
-   res.status(200).json(order);
+const viewOrder = async (req, res) => {
+    try {
+        const { orderId } = req.params;
+        const order = await Order.findOne({ orderId });
+        if (!order) return res.status(404).json({ message: "Order not found" });
+        res.status(200).json(order);
     }
 
-    catch(err){
-        res.status(400).json({message: "Invalid order ID",
-             error: err.message});
+    catch (err) {
+        res.status(400).json({
+            message: "Invalid order ID",
+            error: err.message
+        });
         res.status
     }
 };
 
-const updateOrder = async(req,res) =>{
-    try{
-        const {orderId} =req.params;
-        const { productId, sellerId, buyerId, soldPrice, quantity, total, deliveryAddressLine1, deliveryAddressLine2, deliveryCity, contactNumber, orderDate, orderTime, orderStatus} = req.body;
-    
-        const order = await Order.findOne({orderId});
+const updateOrder = async (req, res) => {
+    try {
+        const { orderId } = req.params;
+        const { productId, sellerId, buyerId, soldPrice, quantity, total, deliveryAddressLine1, deliveryAddressLine2, deliveryCity, contactNumber, orderStatus } = req.body;
 
-        if(!order){
-            return res.status(404).json({message: "Order not found"});
+        const order = await Order.findOne({ orderId });
+
+        if (!order) {
+            return res.status(404).json({ message: "Order not found" });
         };
 
         const updateOrder = await Order.findOneAndUpdate(
-            {orderId},{orderId, productId, sellerId, buyerId, soldPrice, quantity, total, deliveryAddressLine1, deliveryAddressLine2, deliveryCity, contactNumber, orderDate, orderTime, orderStatus}, {new: true}
+            { orderId }, { orderId, productId, sellerId, buyerId, soldPrice, quantity, total, deliveryAddressLine1, deliveryAddressLine2, deliveryCity, contactNumber, orderStatus }, { new: true }
         );
 
-        res.status(200).json({message: "Order updated successfully", Order: updateOrder});
+        res.status(200).json({ message: "Order updated successfully", Order: updateOrder });
     }
-    catch(err){
-        res.status(500).json({message: "Failed to update order", error: err.message});
+    catch (err) {
+        res.status(500).json({ message: "Failed to update order", error: err.message });
     }
-    };
+};
 
-    const deleteOrder = async(req,res) =>{
-        try{
-            const {orderId} = req.params;
-const order = await Order.findOne({orderId});
-res.status(200).json ({message : "Order deleted successfully", Order: order});
-        }
-        catch(err){
-            res.status(500).json({message: "Failed to delete order", error: err.message});
-        }
-    };
+const deleteOrder = async (req, res) => {
+    try {
+        const { orderId } = req.params;
+        const order = await Order.findOneAndDelete({ orderId });
+        res.status(200).json({ message: "Order deleted successfully", Order: order });
+    }
+    catch (err) {
+        res.status(500).json({ message: "Failed to delete order", error: err.message });
+    }
+};
 
-    module.exports = {createOrder, viewAllOrders, viewOrder, updateOrder, deleteOrder};
+module.exports = { createOrder, viewAllOrders, viewOrder, updateOrder, deleteOrder };
--- backend/modules/orderModel.js
+++ backend/modules/orderModel.js
@@ -1,69 +1,63 @@
-const mongoose = require ('mongoose');
+const mongoose = require('mongoose');
+
 const orderSchema = new mongoose.Schema(
-{
-    orderId:{
-        type: String,
-        required: true,
-        unique: true
-    },
-     productId:{
-            type:String,
-            required:true,
-            unique:true
+    {
+        orderId: {
+            type: String,
+            required: true,
+            unique: true
         },
-     sellerId:{
-            type:String,
-            required:true,
-            unique:true
+        productId: {
+            type: String,
+            required: true
         },
-        buyerId:{
-                type:String,
-                required:true,
-                unique:true
-            },
-        soldPrice:{
-            type:Number,
-            required:true
+        sellerId: {
+            type: String,
+            required: true
         },
-        quantity:{
-            type:Number,
-            required:true
+        buyerId: {
+            type: String,
+            required: true
         },
-        total:{
-            type:Number,
-            required:true
+        soldPrice: {
+            type: Number,
+            required: true
         },
-        deliveryAddressLine1:{
-            type:String,
-            required:true
+        quantity: {
+            type: Number,
+            required: true
         },
-        deliveryAddressLine2:{
-            type:String,
-            required:true
+        total: {
+            type: Number,
+            required: true
         },
-        deliveryCity:{
-            type:String,
-            required:true
+        deliveryAddressLine1: {
+            type: String,
+            required: true
         },
-        contactNumber:{
-            type:String,
-            required:true
+        deliveryAddressLine2: {
+            type: String,
+            required: true
         },
-        orderDate:{
-            type:Date,
+        deliveryCity: {
+            type: String,
+            required: true
+        },
+        contactNumber: {
+            type: String,
+            required: true
+        },
+        orderDate: {
+            type: Date,
             default: Date.now
         },
-        orderTime:{
-            type:String,
-            required:true
-        },
-        orderStatus:{
-            type:String,
-            required:true,
+        orderStatus: {
+            type: String,
+            required: true,
             enum: ['Processing', 'Shipped', 'Delivered', 'Cancelled'],
-            default: 'Pending'
+            default: 'Processing'
         }
-}
+    }
 );
 
-module.exports = mongoose.model("order",orderSchema)
+module.exports = mongoose.model("Order", orderSchema)
