c39a68bc30d2a899c116aebdf49a40d16c80dc5e
RashmiFernando <it22177414@my.sliit.lk>
2025-07-04 11:29:20 +0530

    Updated product model

--- backend/modules/productModel.js
+++ backend/modules/productModel.js
@@ -1,44 +1,37 @@
-const mongoose = require ('mongoose');
+const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema(
     {
-        productId:{
-            type:String,
-            required:true,
-            unique: true            
+        productId: {
+            type: String,
+            required: true,
+            unique: true
         },
-        productName:{
-            type:String,
-            required:true
+        productName: {
+            type: String,
+            required: true
         },
-        pricePerKg:{
-            type:Number,
-            required:true
+        description: {
+            type: String,
+            required: true
         },
-        sellerId:{
-            type:String,
-            required:true,            
+        pricePerKg: {
+            type: Number,
+            required: true
         },
-        sellerName:{
-            type:String,
-            required:true
+        userId: {
+            type: String,
+            required: true,
         },
-        description:{
-            type:String,
-            required:true
+        origin: {
+            type: String,
+            required: true
         },
-        origin:{
-            type:String,
-            required:true
-        },
-        catalogue:{
-            type:String,
-            required:true
-        },
-        productImage:{
-             type: String
-             } 
+        productImage: {
+            type: String,
+            required: true
+        }
     }
 );
 
-module.exports = mongoose.model("Product",productSchema);
+module.exports = mongoose.model("Product", productSchema);
