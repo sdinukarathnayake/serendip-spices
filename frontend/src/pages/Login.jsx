5c982b10e281c7f0cd09ea877ae3123e5a90c417
DR <it22140616@my.sliit.lk>
2025-07-10 12:10:27 +0530

    Base URL pull from .env
    - Created .env file
    - Updated .gitignore file
    - Added base urls to .env

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import apiClient from '../api/apiClient'
 
 export default function Login() {
 
@@ -14,10 +15,7 @@ export default function Login() {
         e.preventDefault();
 
         try {
-            const { data } = await axios.post(
-                "http://localhost:5000/users/login",
-                { username, password }
-            );
+            const { data } = await apiClient.post('/users/login', { username, password });
 
             const { user, token } = data;
             localStorage.setItem("token", token);
--- frontend/src/pages/SellerDashboard.jsx
+++ frontend/src/pages/SellerDashboard.jsx
@@ -1,8 +1,7 @@
 export default function SellerDashboard() {
     return (
         <div className="p-8">
-            <h1 className="text-3xl font-bold">Seller Dashboard</h1>
-            {/* add Seller-specific UI here */}
+            <h1 className="text-3xl font-bold">Seller Dashboard</h1>            
         </div>
     );
 }
