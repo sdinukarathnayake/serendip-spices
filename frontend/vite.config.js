74c6da743271719456bd267b2bf0bd0ed2bdfa82
DR <it22140616@my.sliit.lk>
2025-07-13 18:30:42 +0530

    Update docker files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  server: {
+    host: '0.0.0.0'
+  },
+  
   plugins: [
     react(),
     tailwindcss()
